ac458aba2999fc0d0e5c838e6e4061df01edeae1
Jon Distad <jon.distad@gmail.com>
2011-06-04 12:01:03 -0400

    REPL

--- Parser.hs
+++ Parser.hs
@@ -1,8 +1,8 @@
-
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System
 import Control.Monad
 import Control.Monad.Error
+import IO hiding (try)
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
@@ -14,8 +14,10 @@ readExpr input = case parse parseExpr "lisp" input of
 
 main :: IO ()
 main = do args <- getArgs
-          evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-          putStrLn $ extractValue $ trapError evaled
+          case length args of
+               0 -> runRepl
+               1 -> evalAndPrint $ args !! 0
+               otherwise -> putStrLn "Program takes only 0 or 1 argument"
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -258,3 +260,24 @@ equal [arg1, arg2] = do primitiveEquals <- liftM or $ mapM (unpackEquals arg1 ar
                         eqvEquals <- eqv [arg1, arg2]
                         return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
 equal badArgList = throwError $ NumArgs 2 badArgList
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do result <- prompt
+                               if pred result
+                               then return ()
+                               else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
